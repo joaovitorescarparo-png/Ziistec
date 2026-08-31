97ccfeb1ec6233b804934c64c1aaf1f6c9e5196e
joaovitorescarparo-png <joaovitorescarparo@gmail.com>
2026-08-31 20:38:21 -0300

    fix: return field sales to main technician app

--- src/screens/v2/WorkOrderSaleV2.jsx
+++ src/screens/v2/WorkOrderSaleV2.jsx
@@ -1,1 +1,11 @@
-export { default } from './TechnicianSalesV2';
+import React from 'react';
+import TechnicianSalesV2 from './TechnicianSalesV2';
+
+export default function WorkOrderSaleV2(props) {
+  const voltarAoApp = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.delete('v2');
+    window.location.assign(`${url.pathname}${url.search}${url.hash}`);
+  };
+  return <TechnicianSalesV2 {...props} onClose={voltarAoApp} />;
+}
